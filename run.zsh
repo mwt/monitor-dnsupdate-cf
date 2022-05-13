49d1191fa1e8211e3ffefa257fd5d61437bd7601
Matthew W. Thomas <matthew@mwt.me>
2022-05-13 17:40:55 -0500

    format better and handle timeouts

--- run.zsh
+++ run.zsh
@@ -11,8 +11,11 @@ SCRIPT_DIR=${0:a:h}
 CURRENT_HOST=$(cloudflare_dns_query $ZONE_ID $DNS_ID $TOKEN | jq -r '.result.content')
 
 # check if connection is refused
-curl -ISs "https://${SERVER_A}" > /dev/null
+curl -ISs --connect-timeout 1 "https://${SERVER_A}" > /dev/null
 SERVER_A_CONNECT=$?
+
+# exit if any uncaught error (not including cURL tests)
+set -e
 
 if [[ $SERVER_A_CONNECT == 0 ]] {
     # the connection to server A was successful
@@ -23,19 +26,21 @@ if [[ $SERVER_A_CONNECT == 0 ]] {
         # A is up, but the current CNAME is B
         date_time_echo "${SERVER_A} is back up! Rotating DNS to ${SERVER_A}."
         cloudflare_dns_update "$DOMAIN_MAIN" "$SERVER_A" "$ZONE_ID" "$DNS_ID" "$TOKEN"
+        exit 0
     } else {
         # CNAME is weird; do nothing
         date_time_echo "The connection to the server was successful, but the current CNAME is ${CURRENT_HOST}. Expected ${SERVER_A} or ${SERVER_B}."
         exit 1
     }
-} elif [[ $SERVER_A_CONNECT == 7 ]] {
-    # the connection to A was refused (code 7)
+} elif [[ $SERVER_A_CONNECT == 7 || $SERVER_A_CONNECT == 28 ]] {
+    # the connection to A was refused (code 7) or timed out (code 28)
     if [[ $CURRENT_HOST == $SERVER_A ]] {
         # the current CNAME is A and it is down
         date_time_echo "${SERVER_A} went down!"
-        curl -ISs "https://${SERVER_B}" > /dev/null && {
+        curl -ISs --connect-timeout 1 "https://${SERVER_B}" > /dev/null && {
             date_time_echo "${SERVER_B} is up. Rotating DNS to ${SERVER_B}."
             cloudflare_dns_update "$DOMAIN_MAIN" "$SERVER_B" "$ZONE_ID" "$DNS_ID" "$TOKEN"
+            exit 0
         }
     } elif [[ $CURRENT_HOST == $SERVER_B ]] {
         # A is down, and the CNAME is B (presumably because we changed it)
@@ -47,7 +52,6 @@ if [[ $SERVER_A_CONNECT == 0 ]] {
     }
 } else {
     # curl failed in an unexpected way; do nothing
-    date_time_echo "Connection failed with error $SERVER_A_CONNECT. Expected error 7 or success."
+    date_time_echo "Connection failed with error ${SERVER_A_CONNECT}. Expected error 7, 28, or success."
     exit 1
 }
-
